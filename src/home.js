d2d94fc90161fc1236fadb97d7f2e106f4f6b0c2
ETMC\cmaturana <CMaturana@telemedicineclinic.com>
2023-03-06 15:07:07 +0100

    test

--- src/home.js
+++ src/home.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import MultiScreen from "./screens.js"
+import {useLocation} from "react-router-dom";
 
 function App() {
-
+  const location = useLocation();
+  console.log(window.location.host)
   return (
     <div className='App'>
       <h1>Multy screen.</h1>
-      <button type="button" onClick={() => MultiScreen(["http://localhost:3000/contact", "http://localhost:3000/about"])}>
+      <button type="button" onClick={() => MultiScreen([`${window.location.origin}/contact`, `${window.location.origin}/about`])}>
         Open screens
       </button>
     </div>
